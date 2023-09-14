3a3eb36c2fa37c10dad4ed2d63e312f125091602
morispolanco <morispolanco@gmail.com>
2023-09-14 14:49:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import requests
 import docx2txt
-import os
 
-def translate_document(document, target_language, api_key):
+def translate_document(document, target_language):
+    # Obtener la clave API de los secrets de Streamlit
+    api_key = st.secrets["API_KEY"]
+
     # Configurar la URL de la API de AI Translate
     url = "https://ai-translate.pro/api/{API_KEY}/en-{target}".format(
         API_KEY=api_key,
@@ -17,18 +19,16 @@ def translate_document(document, target_language, api_key):
 
     # Realizar la solicitud POST a la API de AI Translate
     response = requests.post(url, json=data)
-
-    # Resto del código...
+    
+    # Verificar si la solicitud fue exitosa
+    if response.status_code == 200:
+        translated_document = response.json()["translated_text"]
+        return translated_document
+    else:
+        raise ValueError("Error en la solicitud a la API de AI Translate")
 
 def main():
     st.title("Traductor de documentos")
-
-    # Obtener la clave API de AI Translate desde la variable de entorno
-    api_key = os.getenv("API_KEY")
-
-    if api_key is None:
-        st.write("Por favor, configura la variable de entorno API_KEY con tu clave API de AI Translate.")
-        return
 
     # Cargar el documento
     uploaded_file = st.file_uploader("Cargar documento", type=["docx"])
@@ -44,7 +44,7 @@ def main():
         if st.button("Traducir"):
             # Traducir el documento utilizando la clave API de AI Translate
             try:
-                translated_document = translate_document(document, target_language, api_key)
+                translated_document = translate_document(document, target_language)
 
                 # Mostrar el documento traducido
                 st.write("Documento traducido:")
