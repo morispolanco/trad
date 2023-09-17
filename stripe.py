1ac9e83a7634e2848fbfff177116d5c7a1c8700c
morispolanco <morispolanco@gmail.com>
2023-09-17 15:42:43 -0600

    Update stripe.py

--- stripe.py
+++ stripe.py
@@ -1,53 +1,62 @@
 import streamlit as st
+import stripe
 import requests
-from docx import Document
 
-# Obtener la clave secreta de la API desde los secrets de Streamlit
-secret_key = st.secrets["API_KEY"]
+# Configurar la clave secreta de Stripe
+stripe.api_key = st.secrets["STRIPE_SECRET_KEY"]
 
 # URL base de la API de AI Translate
 BASE_URL = "https://ai-translate.pro/api"
 
 # Función para traducir texto
-def translate_text(text, lang_from, lang_to, secret_key):
-    url = f"{BASE_URL}/{secret_key}/{lang_from}-{lang_to}"
+def translate_text(text, lang_from, lang_to):
+    url = f"{BASE_URL}/{lang_from}-{lang_to}"
     headers = {'Content-Type': 'application/json'}
     data = {"text": text}
     response = requests.post(url, headers=headers, json=data)
     if response.status_code == 200:
         result = response.json()["result"]
-        available_chars = response.json()["available_chars"]
-        return result, available_chars
+        return result
     else:
-        return None, None
+        return None
 
 # Título de la aplicación
 st.title("Traductor de Texto")
 
-# Cargar archivo DOCX
-uploaded_file = st.file_uploader("Cargar archivo DOCX", type=["docx"])
+# Entrada de texto
+text = st.text_area("Ingrese el texto a traducir")
 
-# Verificar si se cargó un archivo
-if uploaded_file is not None:
-    # Leer el contenido del archivo DOCX
-    docx = Document(uploaded_file)
-    text = "\n".join([paragraph.text for paragraph in docx.paragraphs])
+# Selección de idiomas
+lang_from = st.selectbox("Seleccione el idioma de origen:", ["en", "es"])
+lang_to = st.selectbox("Seleccione el idioma de destino:", ["en", "es"])
 
-    # Mostrar el contenido del archivo
-    st.text_area("Contenido del archivo", value=text)
+# Campo de entrada para la información de pago
+payment_info = st.text_input("Ingrese su información de pago (tarjeta de crédito)")
 
-    # Selección de idiomas
-    lang_from = st.selectbox("Seleccione el idioma de origen:", ["en", "es"])
-    lang_to = st.selectbox("Seleccione el idioma de destino:", ["en", "es"])
+# Botón para procesar el pago
+if st.button("Procesar Pago"):
+    try:
+        # Crear una carga de pago en Stripe
+        payment_intent = stripe.PaymentIntent.create(
+            amount=1000,  # Monto en centavos (ejemplo: $10.00)
+            currency="usd",
+            payment_method_types=["card"],
+        )
 
-    # Botón para traducir
-    if st.button("Traducir"):
-        if secret_key:
-            translation, available_chars = translate_text(text, lang_from, lang_to, secret_key)
-            if translation:
-                st.success(f"Texto traducido: {translation}")
-                st.info(f"Caracteres disponibles: {available_chars}")
-            else:
-                st.error("Error al traducir el texto. Verifique su clave secreta o intente nuevamente.")
+        # Confirmar el pago
+        stripe.PaymentIntent.confirm(payment_intent.id, payment_method=payment_info)
+
+        # Si el pago es exitoso, realizar la traducción del texto
+        translation = translate_text(text, lang_from, lang_to)
+        if translation:
+            st.success(f"Texto traducido: {translation}")
         else:
-            st.error("La clave secreta 'API_KEY' no está configurada en los secrets de Streamlit. Configúrela primero.")
+            st.error("Error al traducir el texto. Verifique su conexión a Internet o intente nuevamente.")
+
+    except stripe.error.CardError as e:
+        # Si hay un error con la tarjeta de crédito, mostrar un mensaje de error
+        st.error(f"Error al procesar el pago: {e.error.message}")
+
+    except Exception as e:
+        # Si hay un error inesperado, mostrar un mensaje de error
+        st.error(f"Error inesperado: {str(e)}")
