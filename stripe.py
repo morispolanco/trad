301b6de97d69130cee50f5b424594f4afd1d50e2
morispolanco <morispolanco@gmail.com>
2023-09-17 16:05:26 -0600

    Update stripe.py

--- stripe.py
+++ stripe.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import stripe
 import requests
-import nltk
 
 # Configurar la clave secreta de Stripe
 stripe.api_key = st.secrets["STRIPE_SECRET_KEY"]
@@ -9,32 +8,8 @@ stripe.api_key = st.secrets["STRIPE_SECRET_KEY"]
 # URL base de la API de AI Translate
 BASE_URL = "https://ai-translate.pro/api"
 
-# Función para contar las palabras en el texto
-def count_words(text):
-    words = nltk.word_tokenize(text)
-    return len(words)
-
-# Función para traducir texto
-def translate_text(text, lang_from, lang_to):
-    url = f"{BASE_URL}/{lang_from}-{lang_to}"
-    headers = {'Content-Type': 'application/json'}
-    data = {"text": text}
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code == 200:
-        result = response.json()["result"]
-        return result
-    else:
-        return None
-
 # Título de la aplicación
-st.title("Traductor de Texto")
-
-# Entrada de texto
-text = st.text_area("Ingrese el texto a traducir")
-
-# Selección de idiomas
-lang_from = st.selectbox("Seleccione el idioma de origen:", ["en", "es"])
-lang_to = st.selectbox("Seleccione el idioma de destino:", ["en", "es"])
+st.title("Aplicación de Pago y Traducción")
 
 # Campo de entrada para la información de pago
 payment_info = st.text_input("Ingrese su información de pago (tarjeta de crédito)")
@@ -42,15 +17,9 @@ payment_info = st.text_input("Ingrese su información de pago (tarjeta de crédi
 # Botón para procesar el pago
 if st.button("Procesar Pago"):
     try:
-        # Contar las palabras en el texto
-        word_count = count_words(text)
-
-        # Calcular el costo de la traducción
-        translation_cost = word_count // 1000 * 1.0
-
         # Crear una carga de pago en Stripe
         payment_intent = stripe.PaymentIntent.create(
-            amount=int(translation_cost * 100),  # Monto en centavos
+            amount=1000,  # Monto en centavos (10 dólares)
             currency="usd",
             payment_method_types=["card"],
         )
@@ -58,12 +27,11 @@ if st.button("Procesar Pago"):
         # Confirmar el pago
         stripe.PaymentIntent.confirm(payment_intent.id, payment_method=payment_info)
 
-        # Si el pago es exitoso, realizar la traducción del texto
-        translation = translate_text(text, lang_from, lang_to)
-        if translation:
-            st.success(f"Texto traducido: {translation}")
-        else:
-            st.error("Error al traducir el texto. Verifique su conexión a Internet o intente nuevamente.")
+        # Si el pago es exitoso, redirigir a la aplicación de traducción
+        if payment_intent.status == "succeeded":
+            st.success("Pago exitoso. Redirigiendo a la aplicación de traducción...")
+            translation_url = f"{BASE_URL}/translate"
+            st.markdown(f'<meta http-equiv="refresh" content="0;URL={translation_url}">', unsafe_allow_html=True)
 
     except Exception as e:
         # Si hay un error, mostrar un mensaje de error
