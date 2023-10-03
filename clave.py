f1bf87df58c48c4e8eb05d1b44791d6a0f7adfdb
morispolanco <morispolanco@gmail.com>
2023-10-03 01:56:00 -0600

    Update clave.py

--- clave.py
+++ clave.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import requests
 from docx import Document
-from io import BytesIO
-
-# Cambiar el título en la pestaña del navegador
-st.set_page_config(page_title="AITranslate", layout="centered")
 
 # URL base de la API de AI Translate
 BASE_URL = "https://ai-translate.pro/api"
@@ -22,51 +18,48 @@ def translate_text(text, lang_from, lang_to, secret_key):
     else:
         return None, None
 
-
 # Título de la aplicación
-st.title("AITranslate")
+st.title("AI Translate")
 
 # Agregar título y texto en la parte superior
 st.markdown("## La mejor traducción automática del mundo")
 st.markdown("Las redes neuronales de AITranslate son capaces de captar hasta los más mínimos matices y reproducirlos en la traducción a diferencia de cualquier otro servicio. Para evaluar la calidad de nuestros modelos de traducción automática, realizamos regularmente pruebas a ciegas. En las pruebas a ciegas, los traductores profesionales seleccionan la traducción más precisa sin saber qué empresa la produjo. AITranslate supera a la competencia por un factor de 3:1.")
 
-# Campo de entrada para la clave API
-secret_key = st.text_input("Ingrese su clave API de AITranslate", type="password")
-
-# Explicación sobre cómo obtener la clave API
-st.markdown("Para obtener la clave API de AI Translate, por favor envíe un correo electrónico a info@editorialarje.com.")
+# Ingresar clave API
+secret_key = st.text_input("Ingrese su clave API de AI Translate")
 
 # Cargar archivo DOCX
 uploaded_file = st.file_uploader("Cargar archivo DOCX", type=["docx"])
 
-# Selección de idiomas
-lang_from = st.selectbox("Seleccione el idioma de origen:", ["en", "es"])
-lang_to = st.selectbox("Seleccione el idioma de destino:", ["en", "es"])
+# Verificar si se cargó un archivo
+if uploaded_file is not None:
+    # Leer el contenido del archivo DOCX
+    docx = Document(uploaded_file)
+    text = "\n".join([paragraph.text for paragraph in docx.paragraphs])
 
-# Botón para traducir
-if st.button("Traducir"):
-    if secret_key and uploaded_file is not None:
-        # Leer el contenido del archivo DOCX
-        docx = Document(uploaded_file)
-        text = "\n".join([paragraph.text for paragraph in docx.paragraphs])
+    # Mostrar el contenido del archivo
+    st.text_area("Contenido del archivo", value=text)
 
-        translation, available_chars = translate_text(text, lang_from, lang_to, secret_key)
-        if translation:
-            # Crear un nuevo documento DOCX con la traducción
-            translated_docx = Document()
-            translated_docx.add_paragraph(translation)
+    # Selección de idiomas
+    lang_from = st.selectbox("Seleccione el idioma de origen:", ["en", "es"])
+    lang_to = st.selectbox("Seleccione el idioma de destino:", ["en", "es"])
 
-            # Guardar el documento DOCX en un objeto BytesIO
-            docx_buffer = BytesIO()
-            translated_docx.save(docx_buffer)
-            docx_buffer.seek(0)
+    # Botón para traducir
+    if st.button("Traducir"):
+        if secret_key:
+            translation, available_chars = translate_text(text, lang_from, lang_to, secret_key)
+            if translation:
+                st.success(f"Texto traducido: {translation}")
+                st.info(f"Caracteres disponibles: {available_chars}")
 
-            # Descargar el archivo DOCX
-            st.download_button("Descargar traducción", data=docx_buffer, file_name="traduccion.docx")
-
-            st.success("La traducción se ha guardado en el archivo 'traduccion.docx'")
-            st.info(f"Caracteres disponibles: {available_chars}")
+                # Descargar el resultado en formato DOCX
+                translated_docx = Document()
+                translated_docx.add_paragraph(translation)
+                st.download_button("Descargar traducción", data=translated_docx.save, file_name="traduccion.docx")
+            else:
+                st.error("Error al traducir el texto. Verifique su clave API o intente nuevamente.")
         else:
-            st.error("Error al traducir el texto. Verifique su clave API o intente nuevamente.")
-    else:
-        st.error("Por favor, ingrese su clave API de AI Translate y cargue un archivo DOCX.")
+            st.error("Por favor, ingrese su clave API de AI Translate.")
+
+
+
